07dc33c2cc77373a849e552aec73bebb4644ce8d
mohamedhanafy <mhmdhnfy783@gmail.com>
2025-03-09 15:03:54 +0200

    first updete 8

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/view/home/widget_home.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -10,6 +11,51 @@ class Home extends StatefulWidget {
 class _Home extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart_outlined))
+        ],
+      ),
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 20),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Fruits &\nVegetables",
+                style: TextStyle(
+                    fontSize: 25,
+                    fontFamily: "Vexa",
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+                padding: EdgeInsets.all(20),
+                child: Image.asset("assets/images/panner.png")),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                width: double.infinity,
+                child: GridView.builder(
+                    itemCount: 4,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        mainAxisSpacing: 50,
+                        crossAxisSpacing: 50,
+                        childAspectRatio: 0.9,
+                        crossAxisCount:
+                            MediaQuery.of(context).size.width < 600 ? 2 : 3),
+                    itemBuilder: (context, i) {
+                      return WidgetHome();
+                    }),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/view/home/widget_home.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+class WidgetHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      height: 170,
+      width: 125,
+      decoration: BoxDecoration(
+          border: Border.all(width: 0.4),
+          borderRadius: BorderRadius.circular(10)),
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: 60,
+                width: 60,
+                padding: EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                    color: Colors.amber.withOpacity(0.4),
+                    borderRadius: BorderRadius.circular(100)),
+                child: Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(100)),
+                ),
+              ),
+              Container(
+                  height: 55,
+                  width: 55,
+                  child: Image.asset("assets/images/banana.png")),
+            ],
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "Banana",
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Vexa"),
+            ),
+          ),
+          Container(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "by weight \$1.32 kg",
+              style: TextStyle(
+                  fontSize: 10, color: Colors.grey, fontFamily: "Vexa"),
+            ),
+          ),
+          Spacer(),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "\$",
+                  style: TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "Vexa"),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "1.37",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "Vexa"),
+                ),
+              ),
+              Expanded(child: Container()),
+              Container(
+                height: 30,
+                width: 30,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: const Color.fromARGB(255, 78, 230, 83),
+                ),
+                child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.add,
+                      size: 15,
+                      color: Colors.white,
+                    )),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
